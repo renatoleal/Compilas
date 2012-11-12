1b56dfc00cd03d7b8701eeba4fb17303e5f72540
Bruno Medeiros de Barros <bmb.eng@gmail.com>
2012-11-12 18:19:38 -0200

    atualizando formulas das tabelas de transicao direta

--- src/com/pilas/sintatico/Sintatico.java
+++ src/com/pilas/sintatico/Sintatico.java
@@ -9,7 +9,7 @@ public class Sintatico {
 	
 	private Lexico lexico;
 	private Stack<Estado> pilha_estados = new Stack<Estado>();
-	private Stack<Estado> pilha_submaquinas = new Stack<Estado>();
+	private Stack<Estado> pilha_retorno = new Stack<Estado>();
 	private Stack<TransicaoIndireta> pilha_indiretas = new Stack<TransicaoIndireta>();
 	private Estado estado_atual = new Estado(0, 1);
 	
@@ -27,6 +27,7 @@ public class Sintatico {
 						" -> (M:"+ next_estado.getMaquina() +", E:"+ next_estado.getIndice() +")");
 				this.estado_atual = next_estado;
 				this.pilha_indiretas.clear();
+				this.pilha_retorno.clear();
 				token = lexico.getNextToken();
 			
 			} else {
@@ -39,12 +40,13 @@ public class Sintatico {
 					
 				} else if(this.estado_atual.getTransicoesIndiretas().size() > 0) { // Verifica se existem transições indiretas
 					
-					for (TransicaoIndireta ti : this.estado_atual.getTransicoesIndiretas()) { 
+					for (TransicaoIndireta ti : this.estado_atual.getTransicoesIndiretas()) {
+						this.pilha_retorno.push(new Estado(ti.getEstadoRetorno(), this.estado_atual.getMaquina()));
 						this.pilha_indiretas.push(ti);
 					}
 					
 					TransicaoIndireta transicao = this.pilha_indiretas.pop();
-					Estado estado_retorno = new Estado(transicao.getEstadoRetorno(), this.estado_atual.getMaquina());
+					Estado estado_retorno = this.pilha_retorno.pop();
 					next_estado = new Estado(0, transicao.getSubMaquina());
 					this.pilha_estados.push(estado_retorno);
 					this.estado_atual = next_estado;
@@ -58,7 +60,7 @@ public class Sintatico {
 						this.pilha_estados.pop(); // Descarta estado de retorno da submáquina que falhou
 
 						TransicaoIndireta transicao = this.pilha_indiretas.pop();
-						Estado estado_retorno = new Estado(transicao.getEstadoRetorno(), this.estado_atual.getMaquina());
+						Estado estado_retorno = this.pilha_retorno.pop();
 						next_estado = new Estado(0, transicao.getSubMaquina());
 						this.pilha_estados.push(estado_retorno);
 						this.estado_atual = next_estado;
